e302436f05ee41f2bbeb2ed6a889f1d8289acd49
nits245 <143995604+nits245@users.noreply.github.com>
2024-05-15 21:04:01 +1000

    Delete assign2 directory

--- assign2/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$host = "feenix-mariadb.swin.edu.au";
-	$user = "s104549141"; // your user name
-	$pwd = "JJNRAssignment2"; // your password (date of birth ddmmyy unless changed)
-	$sql_db = "s104549141_db"; // your database
-?>
-	
-	
-	
