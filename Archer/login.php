6dcc402298b5b232045f84397696cd1ef84b4fd3
nits245 <143995604+nits245@users.noreply.github.com>
2024-05-16 06:15:24 +1000

    Update login.php

--- Archer/login.php
+++ Archer/login.php
@@ -2,24 +2,23 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<meta name="description" content="JNNR login"/>
-		<meta name="keywords" content="JNNR"/>
+		<meta name="description" content="admin login"/>
+		<meta name="keywords" content=""/>
 		<meta name="author" content="nits"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>JNNR login login</title>
+		<title>Login Page</title>
 		<link href="styles/style.css" rel="stylesheet">
 	</head>
 	
 	<body>
 		<?php
             include("header.inc");
-            include("menu.inc");
             session_start();
             if (isset($_POST["login"])){
     if (isset($_POST["username"])){$username = trim($_POST["username"]);}
     if (isset($_POST["password"])){$password = trim($_POST["password"]);}
     if (($username=="admin") AND ($password=="admin123")){$_SESSION["login"] = true;
-    header("location: manage.php");
+    header("location: archer.php");
     echo $_SESSION["login"];}
     else{echo"<div class=\"container\">wrong username or password</div>";}
     }
@@ -29,7 +28,7 @@
                 <fieldset>
 			<form action="login.php" method="POST" novalidate="novalidate">
 
-				<h1 id="title">Management login</h1>
+				<h1 id="title">Admin login</h1>
 				<h2 id="subtitle">In order to query database, your identity needs to be verified.</h2>
 
                 <!-- Radio choice: which query admin would like to make -->
